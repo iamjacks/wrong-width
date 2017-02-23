0cef48b0ec3d8ff6e225201c5a83e8655634fc5e
iamjacks <iamjacksspamemail@gmail.com>
2017-02-23 08:39:40 -0800

    Update other.js

--- pages/other.js
+++ pages/other.js
@@ -1,18 +1,55 @@
+import React from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
 
-export default () => (
-  <div className='container'>
-    <h1>Other Page</h1>
-    <p><Link href='/'><a>Go back home</a></Link></p>
-    <style jsx>{`
-      .container {
-        width: 100%;
-        background-color: #f0f0f0;
-        padding: 50px;
-        margin: 0 auto;
-        text-align: center;
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
+export default class extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      width: 0
+    }
+  }
+  onWindowResize = () => {
+    const w = document.querySelector('.global-inner').clientWidth
+    this.setState({
+      width: w
+    }, () => {
+      console.log('width = ', this.state.width)
+    })
+  }
+  componentDidMount () {
+    this.onWindowResize()
+    window.addEventListener('resize', this.onWindowResize)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.onWindowResize)
+  }
+
+  render () {
+    const {width} = this.state
+    return (
+      <div className='global-container'>
+        <Head>
+          <style>{`
+            .global-container {
+              width: 100%;
+              background-color: LightGreen;
+              padding: 50px;
+              margin: 0 auto;
+              text-align: center;
+              box-sizing: border-box;
+            }
+            .global-inner {
+              background-color: rgba(0,0,0,.1);
+            }
+          `}</style>
+        </Head>
+        <div className='global-inner'>
+          <h1>My width is {width}px</h1>
+        </div>
+        <p><Link href='/'><a>Goto: styled-jsx page</a></Link></p>
+      </div>
+    )
+  }
+}
